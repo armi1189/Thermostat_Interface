e46932c903f013f7d04d7c58e9e66bbde28ee1aa
M1lena <stoevami@gmail.com>
2015-04-14 10:08:55 +0100

    it has default temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,8 +1,13 @@
 describe("Thermostat", function() {
-  
+
   thermostat = new Thermostat();
 
   it("has a default temperature of 20 degrees", function() {
     expect(thermostat.temperature).toEqual(20);
+  });
+
+  it("can increase temperature with 'up' button", function() {
+    thermostat.increase
+    expect(thermostat.temperature).toEqual(21);
   });
 });
