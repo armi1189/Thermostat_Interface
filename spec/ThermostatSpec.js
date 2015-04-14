88ccde0abc08e3c22921386075f3d38ba3f6f392
armi1189 <armi1189@gmail.com>
2015-04-14 09:53:57 +0100

    first test

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,8 @@
+describe("Thermostat", function() {
+  
+  thermostat = new Thermostat();
+
+  it("has a default temperature of 20 degrees", function() {
+    expect(thermostat.temperature).toEqual(20);
+  });
+});
