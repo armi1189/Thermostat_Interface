56851f62c47943879c0354a44efd60b52e679904
armi1189 <armi1189@gmail.com>
2015-04-14 11:29:36 +0100

    refactor part 2

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,4 +1,4 @@
-describe("Thermostat", function() {
+describe("Thermostat default values", function() {
 
   beforeEach(function() {
     thermostat = new Thermostat();
@@ -6,6 +6,32 @@ describe("Thermostat", function() {
 
   it("has a default temperature of 20 degrees", function() {
     expect(thermostat.temperature).toEqual(20);
+  });
+
+
+  it("has a 'Power Save' mode that's 'on' by default", function() {
+    expect(thermostat.powerSave).toBe(true);
+  });
+
+  it("has minimum temperature of 10", function() {
+    expect(thermostat.minimum).toEqual(10);
+  });
+
+  it("has a maximum temperature of 25 if Power Save is on", function() {
+    expect(thermostat.maximum).toEqual(25);
+  });
+
+  it("has a maximum temperature of 32 if Power Save is off", function() {
+    thermostat.powerSaveSwitch();
+    expect(thermostat.maximum).toEqual(32);
+  });
+
+});
+
+describe("Thermostat features", function() {
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
   });
 
   it("can increase temperature with 'up' button", function() {
@@ -20,19 +46,6 @@ describe("Thermostat", function() {
     expect(thermostat.temperature).toEqual(newTemperature);
   });
 
-  it("has a 'Power Save' mode that's 'on' by default", function() {
-    expect(thermostat.powerSave).toBe(true);
-  });
-
-  it("has a switch to change 'Power Save' status", function() {
-    thermostat.powerSaveSwitch();
-    expect(thermostat.powerSave).toBe(false);
-  });
-
-  it("has minimum temperature of 10", function() {
-    expect(thermostat.minimum).toEqual(10);
-  });
-
   it("cannot go under his minimum temperature", function() {
     var decreaseTimes = (thermostat.temperature - thermostat.minimum + 1);
     var count = decreaseTimes;
@@ -40,15 +53,6 @@ describe("Thermostat", function() {
       thermostat.decrease();
     };
     expect(thermostat.temperature).toEqual(thermostat.minimum);
-  });
-
-  it("has a maximum temperature of 25 if Power Save is on", function() {
-    expect(thermostat.maximum).toEqual(25);
-  });
-
-  it("has a maximum temperature of 32 if Power Save is off", function() {
-    thermostat.powerSaveSwitch();
-    expect(thermostat.maximum).toEqual(32);
   });
 
   it("cannot go over maximum temperature if Power Save is on", function() {
@@ -70,10 +74,16 @@ describe("Thermostat", function() {
     expect(thermostat.temperature).toEqual(thermostat.maximum);
   });
 
+  it("has a switch to change 'Power Save' status", function() {
+    thermostat.powerSaveSwitch();
+    expect(thermostat.powerSave).toBe(false);
+  });
+
   it("has a reset button to resets the temperature to default temperature", function() {
     thermostat.increase();
     thermostat.resetButton();
     expect(thermostat.temperature).toEqual(thermostat.defaultTemperature);
   });
+
 
 });
